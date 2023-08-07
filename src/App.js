66e2b940b0d7062fe6170c5b61c589a33292c30e
Youness94 <72896129+Youness94@users.noreply.github.com>
2023-08-07 16:41:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown'
+import { marked } from 'marked';
 import './App.css';
 
 function App() {
-  return (
+
+  const [markdown, setMarkdown] = useState(`
+  # H1
+  ## H2
+ 
+  [title](https://www.example.com)
+  \`code\`
+ 
+  **bold text**
+  > blockquote
+  ![alt text](image.jpg)
+  \`\`\`
+    {
+      "firstName": "John",
+      "lastName": "Smith",
+      "age": 25
+    }
+  \`\`\`
+  - First item
+  - Second item
+  - Third item
+
+  `);
+  marked.setOptions({
+   breaks : true
+  })
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea id="editor" value={markdown} onChange={(event)=>{setMarkdown(event.target.value)}} ></textarea>
+      <div id="preview"
+      dangerouslySetInnerHTML={{
+        __html: marked(markdown),
+       }}></div>
     </div>
   );
 }
